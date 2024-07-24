010664c333a6b51414cdd18231aa2365f4796178
Joaquin <FacundoJoaGL@gmail.com>
2024-07-24 01:49:23 -0300

    cambios en el puerto

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const mysql = require("mysql") //  instalar mysql
 exports.db = mysql.createConnection({
     host: "localhost",
     user: "root",
+    port: "5432", //agregado por joaquin
     password: "1234",
     database: "dentista"
 })
